03f31213a8025c7e4015337130007327dccbea51
Hiloliddin <38894285+hiloliddinj@users.noreply.github.com>
2022-09-16 16:50:43 +0500

    added mtscra lib and building - simulator (added from desctop)

--- ios/Runner/Runner-Bridging-Header.h
+++ ios/Runner/Runner-Bridging-Header.h
@@ -1,1 +1,11 @@
 #import "GeneratedPluginRegistrant.h"
+
+#ifndef Header_h
+#define Header_h
+
+#import "MTSCRA.h"
+//#import "HexUtil.h"
+//#import "NSObject+TLVParser.h"
+//#import "optionController.h"
+
+#endif /* Header_h */
